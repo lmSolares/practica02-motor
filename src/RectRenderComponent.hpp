19e370b601b97f6ca697f0eabaab320bfcc6cd5a
Luis Ramos <lmsr.solares@gmail.com>
2026-09-17 19:48:14 -0600

    practica03: add RectRenderComponent header

---
+++ src/RectRenderComponent.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "Component.hpp"
+#include "GameObject.hpp"
+#include "TransformComponent.hpp"
+#include "Vector2.hpp"
+#include <SDL3/SDL.h>
+class RectRenderComponent : public Component {
+public:
+  Vector2 size{60.0f, 60.0f};
+  SDL_Color color{60, 180, 100, 255};
+  RectRenderComponent() = default;
+  RectRenderComponent(Vector2 sz, SDL_Color col) : size(sz), color(col) {}
+  void Render(SDL_Renderer *renderer) override {
+    if (!owner)
+      return;
+    // Consultamos la posición y escala al TransformComponent de
+    nuestra entidad TransformComponent *transform =
+        owner - > GetComponent<TransformComponent>();
+    if (!transform) {
+      return; // No podemos renderizar si la entidad no tiene
+      posición en el mundo
+    }
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_FRect rect{transform->position.x, transform->position.y,
+                   size.x * transform->scale.x, size.y * transform->scale.y};
+    SDL_RenderFillRect(renderer, &rect);
+  }
+};
